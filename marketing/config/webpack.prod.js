eb022007ff92551a71dca4b8cc8bfe10821211dc
gevorgzoroghlyan <gevorg.zoroghlyan@betconstruct.com>
2021-11-25 13:14:24 +0400

    Fix

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+
 const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
