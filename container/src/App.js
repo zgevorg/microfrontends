2b32955a3da34bb86864d25f1d0fdef8af1aa174
gevorgzoroghlyan <gevorg.zoroghlyan@betconstruct.com>
2021-12-02 15:28:40 +0400

    Fix css

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+});
+
 export default () => {
     return (
-    <BrowserRouter>
+    <BrowserRouter generateClassName={generateClassName}>
         <div>
             <Header />
             <MarketingApp />
